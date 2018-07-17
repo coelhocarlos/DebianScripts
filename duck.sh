30deb9b4c8f9febf4aabc2c4fbb54b8a55e41f74
carlos coelho <carloscoelho_@msn.com>
2018-07-17 17:25:22 -0300

    Update duck.sh

--- duck.sh
+++ duck.sh
@@ -1,1 +1,2 @@
 echo url="https://www.duckdns.org/update?domains=coreserver&token=d7f32ee5-7599-451f-b378-a95b4996bd90&ip=" | curl -k -o ~/.scripts/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=ccstudio&token=d7f32ee5-7599-451f-b378-a95b4996bd90&ip=" | curl -k -o ~/.scripts/duck.log -K -
