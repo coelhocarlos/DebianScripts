9175864908d079bc53fed48440e91bda8c45de97
carlos coelho <carloscoelho_@msn.com>
2018-08-23 12:16:39 -0300

    Update debian9php7.sh

--- debian9php7.sh
+++ debian9php7.sh
@@ -63,7 +63,7 @@ apt-get install mysql-server
     sudo systemctl restart apache2
 ###############PHP############################
     sudo apt install php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json 
-    php7.0-gd php7.0-mcrypt php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-mbstring php-xdebug 
+    php7.0-gd php7.0-mcrypt  php7.0-fpm php7.0-intl php-xdebug php7.0-sqlite3 php7.0-gmp php7.0-mbstring
     php7.0-xml php7.0-zip
     sudo systemctl restart apache2 
     sudo apt-get -y install php7.0-opcache php-apcu
